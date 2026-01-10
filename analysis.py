916b13ea14f701367f2ee52091fdfd7cdfcd7918
jdennison70 <joshuadennison556@gmail.com>
2026-01-10 15:57:13 +0000

    Added two functions to visualise sentiment

--- analysis.py
+++ analysis.py
@@ -2,7 +2,9 @@
 Analyse the sentiment of news articles
 """
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 from news_api import get_news
 
 def get_sentiment(articles):
@@ -16,10 +18,10 @@ def get_sentiment(articles):
     sia = SentimentIntensityAnalyzer()
     for article in articles:
         article_content = str(article['title']) + '. ' + str(article['description'])
-        sentiment = sia.polarity_scores(article_content)['compound']
+        sentiment_score = sia.polarity_scores(article_content)['compound']
         date = article['publishedAt'].split('T')[0]
-        date_sentiments.setdefault(date, []).append(sentiment)
-        date_sentiments_list.append((date, sentiment, article_content))
+        date_sentiments.setdefault(date, []).append(sentiment_score)
+        date_sentiments_list.append((date, sentiment_score, article_content))
     return date_sentiments_list
 
 def analyse_sentiment(date_sentiments):
@@ -34,9 +36,68 @@ def analyse_sentiment(date_sentiments):
 
     return scores_df
 
-if __name__ == '__main__':
-    response = get_news('Microsoft', 7)
+def plot_sentiment_overall_bar(scores_df, keyword):
+    """
+    Plots the sentiment analysis
+    """
+    sentiment_counts = scores_df["Judged Sentiment"].value_counts()
+
+    plt.bar(sentiment_counts.index, sentiment_counts.values)
+    plt.title(f"Sentiment Analysis for {keyword}")
+    plt.ylabel("Count")
+    plt.xlabel("Sentiment")
+    plt.show()
+
+def plot_sentiment_by_date_grouped_bar(scores_df, keyword):
+    """
+    Plots the sentiment analysis by date
+    """
+    grouped_df = scores_df.groupby(["Date", "Judged Sentiment"]).size().unstack(fill_value=0)
+
+    sentiment_order = ["Positive", "Neutral", "Negative"]
+
+    available_sentiments= [s for s in sentiment_order if s in grouped_df.columns]
+
+    grouped_df = grouped_df[sentiment_order]
+
+    dates = grouped_df.index
+    x = np.arange(len(dates)) * 1.5
+    width = 0.25
+
+    _, ax = plt.subplots(figsize=(10, 6))
+
+    colours = {'Positive': '#4CAF50', 'Neutral': '#9E9E9E', 'Negative': '#F44336'}
+
+    for i, sentiment in enumerate(available_sentiments):
+        offset = (i - len(available_sentiments)/2 + 0.5) * width
+        rects = ax.bar(x + offset, grouped_df[sentiment], width, label=sentiment,
+                       color=colours.get(sentiment, '#000000'))
+
+
+        ax.bar_label(rects, padding=3)
+
+    ax.set_ylabel('Count')
+    ax.set_xlabel('Date')
+    ax.set_title('Sentiment Count by Date for ' + keyword)
+    ax.set_xticks(x)
+    ax.set_xticks(x)
+    ax.set_xticklabels(dates)
+    ax.legend(title='Sentiment')
+
+    plt.show()
+
+def main():
+    """
+    Main function
+    """
+    search = input("Enter a search term: ")
+    days_searched = 7
+    response = get_news(search, days_searched)
 
     scores = get_sentiment(response)
+    scores_df = analyse_sentiment(scores)
+    plot_sentiment_by_date_grouped_bar(scores_df = scores_df, keyword=search)
 
-    analyse_sentiment(scores)
+if __name__ == '__main__':
+
+    main()
