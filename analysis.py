9d7a3d1c0077473d5757d29022387642404bee76
jdennison70 <joshuadennison556@gmail.com>
2026-01-10 15:00:14 +0000

    Added Function to turn sentiment score into a bucket

--- analysis.py
+++ analysis.py
@@ -22,11 +22,21 @@ def get_sentiment(articles):
         date_sentiments_list.append((date, sentiment, article_content))
     return date_sentiments_list
 
+def analyse_sentiment(date_sentiments):
+    """
+    Turns the sentiment score into Positve, Negative or Neutral.
+    :param date_sentiments: list of sentiments
+    """
+    scores_df = pd.DataFrame(date_sentiments, columns=['Date', 'Sentiment', 'Content'])
+    scores_df["Judged Sentiment"] = "Neutral"
+    scores_df.loc[scores_df["Sentiment"] > 0.05, "Judged Sentiment"] = "Positive"
+    scores_df.loc[scores_df["Sentiment"] < -0.05, "Judged Sentiment"] = "Negative"
+
+    return scores_df
 
 if __name__ == '__main__':
     response = get_news('Microsoft', 7)
 
     scores = get_sentiment(response)
 
-    scores_df = pd.DataFrame(scores, columns = ['Date', 'Sentiment', 'Content'])
-    scores_df.to_csv('scores.csv')
+    analyse_sentiment(scores)
